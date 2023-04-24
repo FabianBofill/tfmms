f2352c4f9a23ec06a6e3d744d8cbbc86d21ad3f6
mikel240 <miguel_real_2@hotmail.com>
2023-04-24 16:22:07 +0200

    format app.js

--- app.js
+++ app.js
@@ -6,19 +6,15 @@ const csv = require('csv-parser');
 const bodyParser = require('body-parser');
 const MongoStore = require('connect-mongo')(session);
 
-
-
-
-
 const app = express();
-
 
 //DB
 const db = require('./config/db').MongoURI
+
 //Conectar Mongo
-mongoose.connect(db, {useNewUrlParser:true})
-.then(()=>console.log('MongoDB conectado...'))
-.catch(err => console.log(err));
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB conectado...'))
+    .catch(err => console.log(err));
 var connection = mongoose.connection;
 
 //Bodyparser
@@ -30,19 +26,19 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true,
-    store   : new MongoStore({
-      mongooseConnection: connection
+    store: new MongoStore({
+        mongooseConnection: connection
     })
-  }));
+}));
 
-  //Connect Flash
+//Connect Flash
 app.use(flash());
 
 //Variables globales
-app.use((req,res,next)=>{
-    res.locals.success_msg=req.flash('success_msg');
-    res.locals.error_msg=req.flash('error_msg');
-    res.locals.error=req.flash('error');
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 
@@ -51,7 +47,6 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/escenario', require('./routes/escenario'));
 app.use('/co2', require('./routes/co2'));
 app.use('/emision', require('./routes/emision'));
-
 
 //Localhost puerto (levantar server)
 const port = 3000;
