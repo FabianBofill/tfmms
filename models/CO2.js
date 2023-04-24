e46af3d73ee049c58dc759454e42aee9bac448bc
mikel240 <miguel_real_2@hotmail.com>
2023-04-24 15:31:56 +0200

    fomat-co2-fixed

--- models/CO2.js
+++ models/CO2.js
@@ -3,25 +3,21 @@ Schema = mongoose.Schema;
 
 
 const Co2Schema = new mongoose.Schema({
-//DATAFLOW,LAST_UPDATE,freq,airpol,nace_r2,unit,geo,TIME_PERIOD,OBS_VALUE,OBS_FLAG
-DATAFLOW: String,
-LAST_UPDATE: String,
-freq: String,
-airpol: String,
-nace_r2: String,
-unit: String,
-geo: String,
-TIME_PERIOD: String,
-OBS_VALUE: String,
-OBS_FLAG: String
+    //DATAFLOW,LAST_UPDATE,freq,airpol,nace_r2,unit,geo,TIME_PERIOD,OBS_VALUE,OBS_FLAG
+    DATAFLOW: String,
+    LAST_UPDATE: String,
+    freq: String,
+    airpol: String,
+    nace_r2: String,
+    unit: String,
+    geo: String,
+    TIME_PERIOD: String,
+    OBS_VALUE: String,
+    OBS_FLAG: String
 });
 
 
 
 
-const CO2=mongoose.model('Co2', Co2Schema);
-<<<<<<< HEAD
-module.exports =CO2;
-=======
-module.exports =CO2;
->>>>>>> 877cfd12088ca76415c7277a62c4fe74e2c75477
+const CO2 = mongoose.model('Co2', Co2Schema);
+module.exports = CO2;
