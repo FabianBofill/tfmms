5d53b22aa1c13cfac19c390439a449860a6213ec
mikel240 <miguel_real_2@hotmail.com>
2023-04-24 15:30:13 +0200

    format-CO2

--- models/CO2.js
+++ models/CO2.js
@@ -3,21 +3,21 @@ Schema = mongoose.Schema;
 
 
 const Co2Schema = new mongoose.Schema({
-    //DATAFLOW,LAST_UPDATE,freq,airpol,nace_r2,unit,geo,TIME_PERIOD,OBS_VALUE,OBS_FLAG
-    DATAFLOW: String,
-    LAST_UPDATE: String,
-    freq: String,
-    airpol: String,
-    nace_r2: String,
-    unit: String,
-    geo: String,
-    TIME_PERIOD: String,
-    OBS_VALUE: String,
-    OBS_FLAG: String
+//DATAFLOW,LAST_UPDATE,freq,airpol,nace_r2,unit,geo,TIME_PERIOD,OBS_VALUE,OBS_FLAG
+DATAFLOW: String,
+LAST_UPDATE: String,
+freq: String,
+airpol: String,
+nace_r2: String,
+unit: String,
+geo: String,
+TIME_PERIOD: String,
+OBS_VALUE: String,
+OBS_FLAG: String
 });
 
 
 
 
-const CO2 = mongoose.model('Co2', Co2Schema);
-module.exports = CO2;
+const CO2=mongoose.model('Co2', Co2Schema);
+module.exports =CO2;
